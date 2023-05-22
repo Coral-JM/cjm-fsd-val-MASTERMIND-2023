e9f991cbda20a3db78e9aa8751eef2165a18f0ed
Coral Jimenez <info.coraljimenez@gmail.com>
2023-05-22 20:10:27 +0200

    Feat: Getting back the colors from session storage. User colors painted in table. Style: Table modified.

--- js/colorStorage.js
+++ js/colorStorage.js
@@ -14,12 +14,12 @@ sessionStorage.setItem("seleccionColores", JSON.stringify(seleccionColores));
 console.log(JSON.parse(sessionStorage.getItem("seleccionColores")));
 };
 
-    //Evento que se ejecuta al cargar la página
+    //Carga los elementos una vez abierta la página
 window.addEventListener("DOMContentLoaded", () => {
-    // Buscar el botón "COMENZAR" en la página
+    //Almacenaje de los colores escogidos por el usuario al apretar COMENZAR (solo guarda los colores de los círculos, no los del input color)
 const botonComenzar = document.querySelector(".btn-cp");
 
-    // Asignar el evento de click al botón para almacenar los colores
+    //Evento de click al botón para almacenar los colores
     if (botonComenzar) {
         botonComenzar.addEventListener("click", almacenarColores);
     }
@@ -28,3 +28,33 @@ const botonComenzar = document.querySelector(".btn-cp");
 //RECUPERACIÓN DE COLORES DE SESSION STORAGE
 
 const coloresGuardados = JSON.parse(sessionStorage.getItem("seleccionColores"));
+
+document.addEventListener("DOMContentLoaded", () => {
+    const circles = document.querySelectorAll(".userColors");
+
+    if (sessionStorage.getItem("seleccionColores")) {
+        const coloresGuardados = JSON.parse(sessionStorage.getItem("seleccionColores"));
+        
+        circles.forEach((circle, index) => {
+        circle.style.backgroundColor = coloresGuardados[index] || "transparent";
+        });
+    }
+});
+
+//OBTENCIÓN DE COMBINACIÓN RANDOM SECRETA
+
+const storedColors = JSON.parse(sessionStorage.getItem("seleccionColores"));
+
+// Deben haber mínimo cuatro colores seleccionados
+if (storedColors.length < 4) {
+    console.log("No hay suficientes colores almacenados.");
+} else {
+  //OBTENCIÓN DE COMBINACIÓN RANDOM
+    const shuffledColors = storedColors.sort(() => Math.random() - 0.5).slice(0, 4);
+
+  // Pintar los círculos con los colores seleccionados
+    const secretCircles = document.getElementsByClassName("secret");
+        Array.from(secretCircles).forEach((circle, index) => {
+        circle.style.backgroundColor = shuffledColors[index];
+    });
+}
